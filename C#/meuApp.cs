436a55c7e3d7c5a315e662763db83ff7bb2cde0b
Andreza Silva <dedezasilva@live.com>
2021-05-20 14:49:19 -0400

    aprendendo C#

---
+++ C#/meuApp.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuApp
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+        string mensagem = "Alo Mundo!";
+        Console.WriteLine(mensagem);
+        Console.ReadKey();
+    }    
+    }
+
+}
