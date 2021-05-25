03220052656816f5ee8e9626089d7d765291cc53
Andreza Silva <dedezasilva@live.com>
2021-05-25 13:33:15 -0400

    aprendendo estrutura de repetição part1

---
+++ 1estrutura-repeticao.js
@@ -0,0 +1,18 @@
+let numero = 3;
+let contador = 0;
+
+console.log("Tabuada de multiplicação - Nº " + numero);
+
+while (contador <= 10){
+    console.log(numero + " x " +contador + " = " + resultado);
+
+    contador++;
+}
+//Tabuada de multiplicação nº 3
+/*
+3 x 0 = 0 
+3 x 1 = 3
+etc
+etc
+*/
+//Resultado impresso pela estrutura de repetição.
